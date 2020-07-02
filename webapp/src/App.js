08c7adfb5ee3e0bbec0a95ed38c7f5e8eb14ca8b
etlow <42794246+etlow@users.noreply.github.com>
2020-07-02 22:58:57 +0800

    Add prebuilt layers

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleSourceChange = this.handleSourceChange.bind(this);
+    this.handlePresetChange = this.handlePresetChange.bind(this);
     this.handleLayerAdd = this.handleLayerAdd.bind(this);
     this.handleLayerRemove = this.handleLayerRemove.bind(this);
     this.handleLayerChange = this.handleLayerChange.bind(this);
@@ -20,15 +21,15 @@ class App extends React.Component {
   }
 
   availableSources = ["citeseer", "cora", "pubmed"];
+  availablePresetNames = ["No preset layers", "Preset"];
   possibleLayers = [
     {layerNum: 0, name: "Dropout", params: {rate: "0.5"}},
     {layerNum: 1, name: "SparseMul", params: {height: "in_dim", width: "16", decay: "true"}},
     {layerNum: 2, name: "GraphSum"},
     {layerNum: 3, name: "ReLU"},
     {layerNum: 4, name: "MatMul", params: {height: "16", width: "out_dim", decay: "false"}},
-    {layerNum: 5, name: "Output"},
     {layerNum: 6, name: "SoftMax"},
-    {layerNum: 7, name: "MaxPool"}
+    {layerNum: 7, name: "MaxPool"},
   ];
   availableLayers = this.possibleLayers.map((data, index) => (
     {id: index, data: this.copyData(data)}
@@ -36,6 +37,24 @@ class App extends React.Component {
 
   handleSourceChange(index) {
     this.setState({source: index});
+  }
+
+  handlePresetChange(index) {
+    let layerData = [{layerNum: 8, name: "Input"}];
+    let layerNames = ["Dropout", "SparseMul", "GraphSum", "ReLU", "Dropout", "MatMul", "GraphSum"];
+    layerData.push(...layerNames.map(name => {
+      let data = {layerNum: 5, name: "Output"};
+      this.availableLayers.forEach(layer => {
+        if (layer.data.name === name) {
+          data = this.copyData(layer.data);
+        }
+      });
+      return data;
+    }));
+    this.setState({
+      preset: index,
+      layers: layerData.map((data, index) => ({id: index, data: this.copyData(data)}))
+    });
   }
 
   handleLayerAdd(layer) {
@@ -66,17 +85,25 @@ class App extends React.Component {
       layers: this.state.layers.map(layer => layer.data)
     };
     const input = JSON.stringify(params);
-    this.setState({text: input});
+    this.setState({text: "Running..."});
 
     const callback = (function () {
       if (xhr.readyState === 4) {
-        this.setState({text: this.state.text + "\n" + xhr.responseText});
+        this.setState({text: xhr.responseText});
+        if (xhr.status === 200) {
+          let response = JSON.parse(xhr.responseText);
+          this.setState({text:
+            `Training accuracy: ${response.train_acc}
+            Testing accuracy: ${response.test_acc}
+            Time / Epoch: ${response.avg_train_time}`
+          });
+        }
         console.log(xhr);
       }
     }).bind(this);
 
     const xhr = new XMLHttpRequest(),
-      url = "http://xtrap100.d2.comp.nus.edu.sg:4002/";
+      url = "https://xtrap100.d2.comp.nus.edu.sg:4002/gcn";
     xhr.open("POST", url);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = callback;
@@ -86,7 +113,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Source source={this.state.source} onChange={this.handleSourceChange} sources={this.availableSources} />
+        <Source
+          source={this.state.source} onSourceChange={this.handleSourceChange} sources={this.availableSources}
+          preset={this.state.preset} onPresetChange={this.handlePresetChange} presetLayers={this.availablePresetNames}
+        />
         <LayerList layers={this.state.layers} onLayerClick={this.handleLayerRemove} suffix="2.png" hover="×" title="Current layers" onLayerChange={this.handleLayerChange} />
         <Submit onClick={this.handleSubmit} text={this.state.text} />
         <LayerList layers={this.availableLayers} onLayerClick={this.handleLayerAdd} suffix="1.jpg" hover="+" title="Available layers" />
--- webapp/src/Source.js
+++ webapp/src/Source.js
@@ -5,10 +5,15 @@ class Source extends React.Component {
   constructor(props) {
     super(props);
     this.handleSourceChange = this.handleSourceChange.bind(this);
+    this.handlePresetChange = this.handlePresetChange.bind(this);
   }
 
   handleSourceChange(ev) {
-    this.props.onChange(ev.target.value);
+    this.props.onSourceChange(ev.target.value);
+  }
+
+  handlePresetChange(ev) {
+    this.props.onPresetChange(ev.target.value);
   }
 
   render() {
@@ -18,6 +23,12 @@ class Source extends React.Component {
         <select onChange={this.handleSourceChange} value={this.props.source}>
           {this.props.sources.map((source, index) => (
             <option value={index} key={index}>{source}</option>
+          ))}
+        </select>
+        <h2>Preset layers</h2>
+        <select onChange={this.handlePresetChange} value={this.props.preset}>
+          {this.props.presetLayers.map((preset, index) => (
+            <option value={index} key={index}>{preset}</option>
           ))}
         </select>
       </div>
